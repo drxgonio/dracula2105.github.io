2e6338e8029310f4529e5a27c7ba73af90c15d7f
dracula <vantriphan2105@gmail.com>
2020-09-29 17:56:03 +0700

    bad boy

--- js/timer.js
+++ js/timer.js
@@ -1,28 +1,47 @@
-setInterval(function(){
+setInterval(function() {
     var date = new Date();
     var format = "YYYY-MMM-DD DDD";
-    dateConvert(date,format)
-  }, 1);
-  
-  function dateConvert(dateobj,format){
-   var year = dateobj.getFullYear();
-   var month= ("0" + (dateobj.getMonth()+1)).slice(-2);
-   var date = ("0" + dateobj.getDate()).slice(-2);
-   var hours = ("0" + dateobj.getHours()).slice(-2);
-   var minutes = ("0" + dateobj.getMinutes()).slice(-2);
-   var seconds = ("0" + dateobj.getSeconds()).slice(-2);
-   var day = dateobj.getDay();
-   var months = ["1","2","3","4","5","6","7","8","9","10","11","12"];
-   var converted_date = "";
-  
-  switch(format){
-   case "YYYY-MM-DD":
-    converted_date = year + "-" + month + "-" + date;
-    break;
-  case "YYYY-MMM-DD DDD":
-    converted_date = year + "-" + months[parseInt(month)-1] + "-" + date
-    + " " + hours + ":" + minutes + ":" + seconds;
-    break;
-  }
-  document.getElementById("example").innerHTML = converted_date;
-  }
+    dateConvert(date, format)
+}, 1);
+
+function dateConvert(dateobj, format) {
+    var year = dateobj.getFullYear();
+    var month = ("0" + (dateobj.getMonth() + 1)).slice(-2);
+    var date = ("0" + dateobj.getDate()).slice(-2);
+    var hours = ("0" + dateobj.getHours()).slice(-2);
+    var minutes = ("0" + dateobj.getMinutes()).slice(-2);
+    var seconds = ("0" + dateobj.getSeconds()).slice(-2);
+    var day = dateobj.getDay();
+    var months = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+    var converted_date = "";
+
+    switch (format) {
+        case "YYYY-MM-DD":
+            converted_date = year + "-" + month + "-" + date;
+            break;
+        case "YYYY-MMM-DD DDD":
+            converted_date = year + "-" + months[parseInt(month) - 1] + "-" + date +
+                " " + hours + ":" + minutes + ":" + seconds;
+            break;
+    }
+    document.getElementById("example").innerHTML = converted_date;
+    document.getElementById("example1").innerHTML = converted_date;
+    document.getElementById("example2").innerHTML = converted_date;
+}
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("right");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}

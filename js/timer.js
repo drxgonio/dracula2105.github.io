e7ec0cc1d21714920f655ba5ad40285f5dae43b5
TriPV1 <tripv1@unifiedpost.com>
2021-05-27 22:00:32 +0700

    pp

--- js/timer.js
+++ js/timer.js
@@ -1,8 +1,16 @@
 setInterval(function() {
     var date = new Date();
+    var x = document.getElementById("gallery");
+    var mydate = new Date('2021-05-28');
+  if (mydate.getTime() <= date.getTime()) {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
     var format = "YYYY-MMM-DD DDD";
-    dateConvert(date, format)
-}, 1);
+    dateConvert(date, format);
+    
+}, 2);
 
 function dateConvert(dateobj, format) {
     var year = dateobj.getFullYear();
